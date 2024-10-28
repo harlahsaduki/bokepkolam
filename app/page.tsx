25ee1be48a709be37fbe82212a873156b79308f8
harlahsaduki <163113239+harlahsaduki@users.noreply.github.com>
2024-10-28 15:51:58 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -18,11 +18,11 @@ export default async function Home({
         	{query ? (
     return (
         <div className="md:my-2">
-            	<h1>{SITENAME}</h1><h2>Video Bokep Indo Viral Terbaru</h2>
+            	<h1>{SITENAME}</h1> <h2>Video Bokep Indo Viral Terbaru</h2>
                 <SearchCardList query={query} banner />
             ) : (
                 <CardList page={page} per_page={per_page} fld_id={fld_id} />
-            )} <p>{SITENAME} merupakan situs website hiburan dewasa 18+ Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Tante Live Paksa Bokep Abg Jepang Jav Barat Korea China Terbaru nonton video bokep indo yang menyediakan akses ribuan video porno Indonesia terbaru yang bisa anda nikmati secara gratis jadi jangan lupa untuk bookmark ya.</p>
+            )}<p>{SITENAME} merupakan situs website hiburan dewasa 18+ Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Viral Sma Tante Live Paksa Bokep Abg Jepang Jav Barat Korea China Terbaru</p><p>nonton video bokep indo yang menyediakan akses ribuan video porno Indonesia terbaru yang bisa anda nikmati secara gratis jadi jangan lupa untuk bookmark ya.</p>
 	</div>
     );
 }
